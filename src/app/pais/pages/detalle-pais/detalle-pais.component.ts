89becc33f4d049984c9aabe1fea3f091888c1149
Juan Berzosa <getinfo@ono.com>
2021-06-17 21:29:46 +0200

    Create pages for pais module

---
+++ src/app/pais/pages/detalle-pais/detalle-pais.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-pais',
+  templateUrl: './detalle-pais.component.html',
+  styles: [
+  ]
+})
+export class DetallePaisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pages/por-region/por-region.component';
+import { DetallePaisComponent } from './pages/detalle-pais/detalle-pais.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PorCapitalComponent,
+    PorPaisComponent,
+    PorRegionComponent,
+    DetallePaisComponent
+  ],
   imports: [
     CommonModule
   ]
