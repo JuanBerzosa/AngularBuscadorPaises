aebb3b44adeaa09e6fa3837809108c569d5ffcb8
Juan Berzosa <getinfo@ono.com>
2021-06-18 10:16:08 +0200

    Add some routes to AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PorPaisComponent,
+    // todas las demas rutas tambien tienen un '' antes del /, por eso se pone esto:
+    pathMatch: 'full',
+  },
+  {
+    path: '/pais',
+    component: PorPaisComponent,
+  },
+  {
+    path: '/region',
+    component: PorRegionComponent,
+  },
+  {
+    path: '/capital',
+    component: PorCapitalComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   declarations: [],
