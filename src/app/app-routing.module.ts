fa72be54d8dcc4a0669ca8640c9590d3f97d4e45
Juan Berzosa <getinfo@ono.com>
2021-06-18 10:11:13 +0200

    Create empty appRoutingModule

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  providers: [],
+  bootstrap: [],
+})
+export class AppRoutingModule {}
