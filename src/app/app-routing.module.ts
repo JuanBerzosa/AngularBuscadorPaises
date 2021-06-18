b508773241e9d829b9407fb51a870c49911d5aa6
Juan Berzosa <getinfo@ono.com>
2021-06-18 10:41:33 +0200

    Add country detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetallePaisComponent } from './pais/pages/detalle-pais/detalle-pais.component';
 import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
@@ -22,6 +23,10 @@ const routes: Routes = [
   {
     path: 'capital',
     component: PorCapitalComponent,
+  },
+  {
+    path: 'pais/:id',
+    component: DetallePaisComponent,
   },
   {
     path: '**',
